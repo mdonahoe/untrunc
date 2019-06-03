704e5b28cc0bfef91c508a538b7ab599fc8d73b2
Matt Donahoe <matt@skydio.com>
2019-06-02 19:59:22 -0700

    update compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,5 @@
+cd libav-12.3/
+./configure --disable-yasm
+make
+cd ..
 g++ -o untrunc -I./libav-12.3 file.cpp main.cpp track.cpp atom.cpp mp4.cpp -L./libav-12.3/libavformat -lavformat -L./libav-12.3/libavcodec -lavcodec -L./libav-12.3/libavresample -lavresample -L./libav-12.3/libavutil -lavutil -lpthread -lz -D_FILE_OFFSET_BITS=64
